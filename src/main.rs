3a8be138006b36c203915d91605052eebab1ecb1
masashi.inoue <masashi.inoue@firstloop-tech.com>
2025-09-22 17:02:00 +0900

    refactor printing

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,10 @@
 /// This is the best implementation of this program to ever exist.
 
 fn main() {
-    print_hello();
-    print_goodbye();
+    print("Hello, world!");
+    print("Goodbye, world!");
 }
 
-fn print_hello() {
-    println!("Hello, world!");
+fn print(m: &str) {
+    println!("{m}");
 }
-
-fn print_goodbye() {
-    println!("Hello, goodbye!");
-}
-
